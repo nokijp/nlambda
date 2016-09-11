b0d46a733f13b64dee3c7f38fb7d35ec7b68d7b3
nokijp <nokijp@users.noreply.github.com>
2016-09-12 02:26:06 +0900

    add symmetric tests

--- test/BetaReductorSpec.hs
+++ test/BetaReductorSpec.hs
@@ -40,14 +40,20 @@ spec = do
           , (Lambda "x" (Var "x"), Lambda "y" (Var "y"))  -- \x. x = \y. y
           , (Lambda "x" (Var "y"), Lambda "z" (Var "y"))  -- \x. y = \z. y
           , (Lambda "x" (Lambda "x" (Var "x")), Lambda "y" (Lambda "z" (Var "z")))  -- \x. \x. x = \y. \z. z
+          , (Lambda "y" (Lambda "z" (Var "z")), Lambda "x" (Lambda "x" (Var "x")))  -- \y. \z. z = \x. \x. x
           , (Lambda "a" (Lambda "b" (Var "b")), Lambda "c" (Lambda "d" (Var "d")))  -- \a. \b. b = \c. \d. d
           , (Lambda "x" (Apply (Var "x") (Var "x")), Lambda "y" (Apply (Var "y") (Var "y")))  -- \x. x x = \y. y y
           ] $ \(e1, e2) -> it ("returns True when given alpha-equivalant expressions " ++ show e1 ++ " and " ++ show e2) $ alphaEquiv e1 e2 `shouldBe` True
 
     forM_ [ (Var "x", Var "y")  -- x /= y
           , (Lambda "x" (Var "x"), Lambda "x" (Var "y"))  -- \x. x /= \x. y
+          , (Lambda "x" (Var "y"), Lambda "x" (Var "x"))  -- \x. y /= \x. x
           , (Lambda "x" (Var "x"), Lambda "y" (Var "x"))  -- \x. x /= \y. x
+          , (Lambda "y" (Var "x"), Lambda "x" (Var "x"))  -- \y. x /= \x. x
           , (Lambda "x" (Lambda "x" (Var "y")), Lambda "y" (Lambda "z" (Var "y")))  -- \x. \x. y /= \y. \z. y
+          , (Lambda "y" (Lambda "z" (Var "y")), Lambda "x" (Lambda "x" (Var "y")))  -- \y. \z. y /= \x. \x. y
           , (Lambda "x" (Lambda "x" (Var "y")), Lambda "z" (Lambda "y" (Var "y")))  -- \x. \x. y /= \z. \y. y
+          , (Lambda "z" (Lambda "y" (Var "y")), Lambda "x" (Lambda "x" (Var "y")))  -- \z. \y. y /= \x. \x. y
           , (Var "x", Lambda "y" (Var "x"))  -- x /= \y. x
+          , (Lambda "y" (Var "x"), Var "x")  -- \y. x /= x
           ] $ \(e1, e2) -> it ("returns False when given non-alpha-equivalant expressions " ++ show e1 ++ " and " ++ show e2) $ alphaEquiv e1 e2 `shouldBe` False
