6f5ca643aa5d2432a7ed5cc63d97593e90fc1f6e
nokijp <nokijp@users.noreply.github.com>
2016-09-12 01:11:52 +0900

    add reduct

--- src/BetaReductor.hs
+++ src/BetaReductor.hs
@@ -1,6 +1,35 @@
 module BetaReductor
-    ( test
-    ) where
+  ( Lambda(..)
+  , reduct
+  ) where
 
-test :: a -> Int
-test _ = 1
+import Control.Applicative
+import qualified Data.List as L
+import qualified Data.Set as S
+
+data Lambda = Var String | Lambda String Lambda | Apply Lambda Lambda deriving (Eq, Show)
+
+freeVariables :: Lambda -> S.Set String
+freeVariables (Var s) = S.singleton s
+freeVariables (Lambda s e) = S.delete s $ freeVariables e
+freeVariables (Apply e1 e2) = freeVariables2 e1 e2
+
+freeVariables2 :: Lambda -> Lambda -> S.Set String
+freeVariables2 e1 e2 = freeVariables e1 `S.union` freeVariables e2
+
+newVariable :: S.Set String -> String -> String
+newVariable useds base = let candidates = iterate (++ "'") base
+                         in (\(Just x) -> x) $ L.find (`S.notMember` useds) candidates
+
+apply :: String -> Lambda -> Lambda -> Lambda
+apply s1 (Var s2) e | s1 == s2  = e
+                    | otherwise = Var s2
+apply s1 (Lambda s2 e1) e2 = let s2' = newVariable (freeVariables2 e1 e2) s2
+                             in Lambda s2' $ apply s1 (apply s2 e1 (Var s2')) e2
+apply s (Apply e1 e2) e3 = Apply (apply s e1 e3) (apply s e2 e3)
+
+reduct :: Lambda -> Maybe Lambda
+reduct (Var _) = Nothing
+reduct (Lambda s e) = Lambda s <$> reduct e
+reduct (Apply (Lambda s e1) e2) = Just $ apply s e1 e2
+reduct (Apply e1 e2) = ((`Apply` e2) <$> reduct e1) <|> (Apply e1 <$> reduct e2)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,26 @@
 import Test.Hspec
+
 import BetaReductor
+import Control.Monad
 
 main :: IO ()
 main = hspec $ do
-  describe "test" $ do
-    it "returns 1" $
-      test 3 `shouldBe` 1
+  describe "reduct" $ do
+    forM_ [ (Apply (Lambda "x" (Var "x")) (Var "x"), Var "x")  -- (\x. x) x -> x
+          , (Apply (Lambda "x" (Var "y")) (Var "x"), Var "y")  -- (\x. y) x -> y
+          , (Apply (Lambda "x" (Lambda "x" (Var "x"))) (Var "x"), Lambda "x'" (Var "x'"))  -- (\x. (\x. x)) x -> \x'. x'
+          , (Apply (Lambda "y" (Lambda "x" (Var "x"))) (Var "x"), Lambda "x'" (Var "x'"))  -- (\y. (\x. x)) x -> \x'. x'
+          , (Apply (Lambda "x" (Lambda "y" (Var "x"))) (Var "x"), Lambda "y" (Var "x"))  -- (\x. (\y. x)) x -> \y. x
+          , (Apply (Var "x") (Apply (Lambda "y" (Var "y")) (Var "z")), Apply (Var "x") (Var "z"))  -- x ((\y. y) z) -> x z
+          , (Lambda "x" (Apply (Lambda "y" (Var "y")) (Var "z")), Lambda "x" (Var "z"))  -- (\x. (\y. y) z) -> \x. z
+          , (Apply (Lambda "y" (Lambda "x" (Apply (Var "x") (Var "y")))) (Var "x"), Lambda "x'" (Apply (Var "x'") (Var "x")))  -- (\y. (\x. x y)) x -> \x'. x' x
+          , (Apply (Lambda "y" (Lambda "x" (Apply (Var "x'") (Var "y")))) (Var "x"), Lambda "x''" (Apply (Var "x'") (Var "x")))  -- (\y. (\x. x' y)) x -> \x''. x' x
+          , (Apply (Lambda "y" (Lambda "x'" (Apply (Var "x'") (Var "y")))) (Var "x"), Lambda "x''" (Apply (Var "x''") (Var "x")))  -- (\y. (\x'. x' y)) x -> \x''. x'' x
+          , (Apply (Lambda "x'" (Lambda "x" (Apply (Var "x''") (Var "x'")))) (Var "x"), Lambda "x'''" (Apply (Var "x''") (Var "x")))  -- (\x'. (\x. x'' x')) x -> \x'''. x'' x
+          ] $ \(expression, result) -> it ("reducts " ++ show expression) $ reduct expression `shouldBe` Just result
+
+    forM_ [ Var "x"
+          , Lambda "x" (Var "x")
+          , Lambda "x" (Var "y")
+          , Apply (Var "x") (Var "x")
+          ] $ \expression -> it ("can't reduct " ++ show expression) $ reduct expression `shouldBe` Nothing
