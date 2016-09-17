5ec6f770f06afe35ec47b68d3512226124758958
nokijp <nokijp@users.noreply.github.com>
2016-09-17 18:53:48 +0900

    add a test case

--- test/BetaReductorSpec.hs
+++ test/BetaReductorSpec.hs
@@ -24,6 +24,7 @@ spec = do
           , (Apply (Lambda "x" (Lambda "y" (Var "x"))) (Var "x"), Lambda "y" (Var "x"))  -- (\x. \y. x) x -> \y. x
           , (Apply (Var "x") (Apply (Lambda "y" (Var "y")) (Var "z")), Apply (Var "x") (Var "z"))  -- x ((\y. y) z) -> x z
           , (Lambda "x" (Apply (Lambda "y" (Var "y")) (Var "z")), Lambda "x" (Var "z"))  -- \x. (\y. y) z -> \x. z
+          , (Apply (Lambda "x" (Lambda "y" (Var "x"))) (Lambda "y" (Var "y")), Lambda "y" (Lambda "y" (Var "y")))  -- (\x y. x) (\y. y)
           , (Apply (Lambda "y" (Lambda "x" (Apply (Var "x") (Var "y")))) (Var "x"), Lambda "x'" (Apply (Var "x'") (Var "x")))  -- (\y. \x. x y) x -> \x'. x' x
           , (Apply (Lambda "y" (Lambda "x" (Apply (Var "x'") (Var "y")))) (Var "x"), Lambda "x''" (Apply (Var "x'") (Var "x")))  -- (\y. \x. x' y) x -> \x''. x' x
           , (Apply (Lambda "y" (Lambda "x'" (Apply (Var "x'") (Var "y")))) (Var "x"), Lambda "x''" (Apply (Var "x''") (Var "x")))  -- (\y. \x'. x' y) x -> \x''. x'' x
